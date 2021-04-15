afe081545d726aeff81446de374186361d5f5f96
Salih <salih285@gmail.com>
2021-04-15 21:27:55 +0200

    v1

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # cpm_script
-A simple cmake package manager script
+
+I made this script for managing packages built with cmake;
+
+- To be able to easily uninstall packages.
+- To be able to move compiled packages across other computers (which are using this script)
+- To be able to keep installed packages across distro updates or full re-install of the OS.
+- To be able to see how much disk space these packages are using
+
+## Install
+
+```bash
+BIND_FOLDER="/cpm"
+INSTALL_FOLDER="$HOME/.cpm"
+
+$ sudo mkdir "$BIND_FOLDER"
+$ sudo chown "$USER":"$USER" "$BIND_FOLDER"
+$ mkdir "$INSTALL_FOLDER"
+
+$ sudo mount --bind "$INSTALL_FOLDER" "$BIND_FOLDER"
+```
+
+- Run the following command and copy its output. 
+
+```bash
+$ echo "$INSTALL_FOLDER $BIND_FOLDER none defaults,bind 0 0"
+```
+
+- Run `sudo nano /etc/fstab` and place the text you copied at the end of the file.
+- To install the script, run the following commands:
+
+```bash
+$ cp cpm_script.sh ~/.cpm_script.sh
+$ echo "source ~/.cpm_script.sh" >> ~/.bashrc
+```
+
--- cpm_script.sh
+++ cpm_script.sh
@@ -1,54 +1,64 @@
-CPM_ROOT="$HOME/cpm"
-SYMLINK_ROOT="$CPM_ROOT/root"
+CPM_PATH="/cpm"
+CPM_CURRENT_ROOT="$CPM_PATH/root"
 SYMLINK_PARAM="-ans" # overwrite symlinks: -as , don't overwrite symlinks -ans
 CMAKE_JOBS=4
 CMAKE_BUILD_TYPE="Release"
 CMAKE_C_FLAGS="-march=native -mtune=native"
 CMAKE_CXX_FLAGS="-march=native -mtune=native"
 
-export LD_LIBRARY_PATH="$CPM_ROOT/root/lib:$LD_LIBRARY_PATH"
-export CMAKE_PREFIX_PATH="$CPM_ROOT/root:$CMAKE_PREFIX_PATH"
-export PATH="$CPM_ROOT/root/bin:$PATH"
+export LD_LIBRARY_PATH="$CPM_CURRENT_ROOT/lib:$LD_LIBRARY_PATH"       # For loading libraries
+export CMAKE_PREFIX_PATH="$CPM_CURRENT_ROOT:$CMAKE_PREFIX_PATH"       # For finding libraries with cmake
+export PATH="$CPM_CURRENT_ROOT/bin:$PATH"                             # For adding binaries for easy access
 
-function cpm_make(){
-    set -e
-    cpm_current_dir=${PWD##*/}                   # get current dir
-    cpm_current_dir=$(echo "${cpm_current_dir// /}") # remove whitespaces
-    cpm_root_dir="$CPM_ROOT/root"
-    cpm_pkg_dir="$CPM_ROOT/pkgs/$cpm_current_dir"
-    mkdir -p "$cpm_root_dir"
+
+function cpm_make_install(){
+    pwd_dir=${PWD##*/}                    # get current dir
+    pwd_dir=$(echo "${pwd_dir// /}")      # remove whitespaces
+    cpm_pkg_dir="$CPM_PATH/pkgs/$pwd_dir"
+    mkdir -p "$CPM_CURRENT_ROOT"
     mkdir -p "$cpm_pkg_dir"
 
     # build package
     mkdir -p build
     cd build
-    cmake .. -DCMAKE_INSTALL_PREFIX="$cpm_pkg_dir" -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS"
-    make -j "$CMAKE_JOBS"
-    cd ..
+    cmake .. -DCMAKE_INSTALL_PREFIX="$cpm_pkg_dir" -DCMAKE_BUILD_TYPE="$CMAKE_BUILD_TYPE" -DCMAKE_C_FLAGS="$CMAKE_C_FLAGS" -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" &&
+    make -j "$CMAKE_JOBS" install &&
+    cd .. &&
+    cp "$SYMLINK_PARAM" "$cpm_pkg_dir/"* "$CPM_CURRENT_ROOT/" # todo add check
 }
 
-function cpm_install(){
-    set -e
-    cpm_current_dir=${PWD##*/}                   # get current dir
-    cpm_current_dir=$(echo "${cpm_current_dir// /}") # remove whitespaces
-    cpm_root_dir="$CPM_ROOT/root"
-    cpm_pkg_dir="$CPM_ROOT/pkgs/$cpm_current_dir"
-    mkdir -p "$cpm_root_dir"
-    mkdir -p "$cpm_pkg_dir"
 
-    cd build
-    make -j "$CMAKE_JOBS" install
-    cd ..
-    cp "$SYMLINK_PARAM" "$cpm_pkg_dir/"* "$cpm_root_dir/"
+function cpm_refresh(){
+    echo "Purging $CPM_CURRENT_ROOT"
+    rm -rf "$CPM_CURRENT_ROOT"
+    mkdir -p "$CPM_CURRENT_ROOT"
+
+    for cpm_pkg_dir in "$CPM_PATH/pkgs"/*; do
+        echo "Linking $cpm_pkg_dir"
+        cp "$SYMLINK_PARAM" "$cpm_pkg_dir/"* "$CPM_CURRENT_ROOT/"
+    done
+
+    echo "Done!"
 }
 
-function cpm_make_install(){
-    set -e
-    cpm_make
-    cpm_install
+function cpm_get_params(){
+    echo ""
+    echo "===== CPM PARAMS ====="
+    echo "CPM_PATH = $CPM_PATH"
+    echo "CPM_CURRENT_ROOT = $CPM_CURRENT_ROOT"
+    echo "SYMLINK_PARAM = $SYMLINK_PARAM"
+    echo "CMAKE_JOBS = $CMAKE_JOBS"
+    echo "CMAKE_BUILD_TYPE = $CMAKE_BUILD_TYPE"
+    echo "CMAKE_C_FLAGS = $CMAKE_C_FLAGS"
+    echo "CMAKE_CXX_FLAGS = $CMAKE_CXX_FLAGS"
+    echo ""
+    echo "===== ENV AFFECTED BY CPM ====="
+    echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+    echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
+    echo "PATH = $PATH"
+    echo ""
 }
 
-function cpm_manager(){
-    # todo:
-    #find "$cpm_root_dir" -xtype l -delete
+function cpm_list_packages(){
+    du -h -d 1  "$CPM_PATH/pkgs" | head -n -1
 }
